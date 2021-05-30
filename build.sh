8c98c7027ab735620a66d42abab654f6508b8e6c
engrvivs <22600345+engrvivs@users.noreply.github.com>
2021-05-30 21:16:46 +0530

    feat(test): catch2 based simple test system

--- build.sh
+++ build.sh
@@ -4,11 +4,12 @@ rm -rf build*/
 
 # Configure
 # <CXX=g++> cmake -S . -B build <-G"Unix Makefiles"> <-DBUILD_TESTING=ON> <-DCMAKE_INSTALL_PREFIX:PATH=install> <-DCMAKE_BUILD_TYPE=Debug> <--trace OR --trace-source="filename" <--trace-expand>>
-CXX=g++ cmake -S . -B buildMake -G"Unix Makefiles"
+CXX=g++ cmake -S . -B buildMake -G"Unix Makefiles" -DBUILD_TESTING=ON
 
 # make
 # cmake --build buildMake -j 4 -v 2> make.out
 cmake --build buildMake -j 4 -v
 
 # check 
+./buildMake/test_snakes-ladders
 ./buildMake/snakes-ladders
---
+++ test/Board.cxx
@@ -0,0 +1,29 @@
+#include "catch.hpp"
+
+#include "Board.hpp"
+
+TEST_CASE(
+    "GIVEN a board and a square WHEN no snake/ladder THEN no change in square",
+    "[Board]") {
+  Board board;
+  REQUIRE(1 == board.landOn(1).target);
+  REQUIRE(38 == board.landOn(38).target);
+  REQUIRE(26 == board.landOn(26).target);
+}
+
+TEST_CASE("GIVEN a board WHEN on ladder THEN land on higher square",
+          "[Board][Ladder]") {
+  Board board;
+  REQUIRE(38 == board.landOn(2).target);
+}
+
+TEST_CASE("GIVEN a board WHEN on snake THEN land on lower square",
+          "[Board][Snake]") {
+  Board board;
+  REQUIRE(26 == board.landOn(47).target);
+}
+
+TEST_CASE("GIVEN a board WHEN on 100 THEN Win", "[Board][Win]") {
+  Board board;
+  REQUIRE(Action::Type::Win == board.landOn(100).type);
+}
